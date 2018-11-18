5e1c5229e1d0a723f7d4a2aecf7f470df9a5ce30
郑军 <zj749122147@outlook.com>
2018-11-18 17:00:45 +0800

    分支中添加一段信息

--- src/com/xincheng/eclipseGit/Test.java
+++ src/com/xincheng/eclipseGit/Test.java
@@ -3,7 +3,7 @@ package com.xincheng.eclipseGit;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("eclipse中使用git上传项目");
+		System.out.println("eclipse中使用git上传");
 	}
 	
 }
---
+++ src/com/xincheng/eclipseGit/TestBranch.java
@@ -0,0 +1,10 @@
+package com.xincheng.eclipseGit;
+
+public class TestBranch {
+	
+	private void mian() {
+		// TODO Auto-generated method stub
+		System.out.println("eclipse 中测试分支 dev branch");
+	}
+	
+}
